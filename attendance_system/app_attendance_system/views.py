cba4ca7232506a45890da271f86bf45e1055dd6b
yogendradhami <yogendradhami6@gmail.com>
2023-04-27 17:49:08 +0545

    student view attendance created successfully.

--- attendance_system/app_attendance_system/views.py
+++ attendance_system/app_attendance_system/views.py
@@ -868,4 +868,25 @@ def StaffviewAttendance(request):
 # Student view Attendance
 
 def StudentViewAttendance(request):
-    return render(request, 'student/student_view_attendance.html')
+    student= Student.objects.get(admin= request.user.id)
+    subjects=Subject.objects.filter(cource=student.cource_id)
+    action = request.GET.get('action')
+
+    get_subject=None
+    attendance_report=None
+    if action is not None:
+        if request.method == 'POST':
+            subject_id=request.POST.get('subject_id')
+            get_subject = Subject.objects.get(id=subject_id)
+
+           
+            attendance_report= Attendance_Report.objects.filter(student_id=student,attendance_id__subject_id=subject_id)
+
+    context = {
+        'subjects':subjects,
+        'action':action,
+        'get_subject':get_subject,
+        'attendance_report':attendance_report,
+
+    }
+    return render(request, 'student/student_view_attendance.html',context)
